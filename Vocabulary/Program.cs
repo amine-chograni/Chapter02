30908cc8cb5e7ff3085a53f3b35ffebd4fc68886
AMINE CHOGRANI <aminechograni@gmail.com>
2025-10-17 09:56:59 +0100

    Mise à jour de Program.cs et Vocabulary.csproj
    Ajout d'un message d'erreur et d'une interpolation de chaîne dans
    `Program.cs`, avec utilisation statique de `System.Console` et
    alias pour `System.Environment`.
    
    Modification de `Vocabulary.csproj` pour supprimer `System.Threading`
    et inclure explicitement `System.Numerics`, `System.Console` (statique)
    et `System.Environment` (alias `Env`), permettant une gestion plus
    précise des espaces de noms.

--- Vocabulary/Program.cs
+++ Vocabulary/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//error version
 Console.WriteLine("Hello, World!");
+
+WriteLine($"Computer named {Env.MachineName} says \"No.\"");
